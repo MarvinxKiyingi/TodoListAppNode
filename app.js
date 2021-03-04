50222ff97ec01a0aba08960567cae2a4eb424e12
marv-ink <marvin96k@gmail.com>
2021-03-04 14:51:38 +0100

    added error if and when the email already exists and a few other minor tweeks

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const chalk = require('chalk'); // Denna navänds för att ändra färg i termin
 
 const express = require('express');
 const app = express();
-
 require('dotenv').config();
 
 const mongoose = require('mongoose');
